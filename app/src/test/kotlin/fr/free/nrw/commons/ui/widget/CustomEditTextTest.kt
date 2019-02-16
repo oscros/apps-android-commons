7213229dc81eaef785c5d4d7bb8963ac7cbc0b99
Felix Eder <felix.eder@gmx.com>
2019-02-16 15:06:36 +0100

    Major work on test

--- app/src/test/kotlin/fr/free/nrw/commons/ui/widget/CustomEditTextTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/ui/widget/CustomEditTextTest.kt
@@ -1,16 +1,56 @@
 package fr.free.nrw.commons.ui.widget
 
+import android.content.Context
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.MotionEvent
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 class CustomEditTextTest {
+    @Mock
+    private lateinit var mEvent:MotionEvent
+    @Mock
+    private lateinit var mContext: Context
+    @Mock
+    private lateinit var mAttrs: AttributeSet
+    @Mock
+    private lateinit var mDrawable: Drawable
+    @Mock
+    private lateinit var mListener: CustomEditText.DrawableClickListener
+
+    private lateinit var mCustomEditText: CustomEditText
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        mEvent.action = MotionEvent.ACTION_DOWN
+        mCustomEditText = CustomEditText(mContext, mAttrs)
+    }
+
+    @After
+    fun tearDown() {
+        for(value in mCustomEditText.testCoverage)
+            println(value)
     }
 
     @Test
-    fun onTouchEvent() {
+    fun onTouchEventTestDrawableLeft() {
+        mCustomEditText.setCompoundDrawables(mDrawable, null, null, null)
+        val rect = Rect()
+
+        Mockito.`when`(mDrawable.bounds).thenReturn(rect)
+        Mockito.`when`(rect.contains(Mockito.anyInt(), Mockito.anyInt()))
+                .thenReturn(true)
+
+        mCustomEditText.onTouchEvent(mEvent)
+
+        Mockito.verify(mListener.onClick(CustomEditText.DrawableClickListener.DrawablePosition.BOTTOM))
     }
 }
