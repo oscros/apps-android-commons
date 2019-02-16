95a26eafd2cbb3566983dca585615ab61ebca7e5
Felix Eder <felix.eder@gmx.com>
2019-02-16 15:34:32 +0100

    Started with FilePickerTest

--- app/src/test/kotlin/fr/free/nrw/commons/filepicker/FilePickerTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/filepicker/FilePickerTest.kt
@@ -1,22 +1,40 @@
 package fr.free.nrw.commons.filepicker
 
+import android.app.Activity
+import android.content.Intent
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 class FilePickerTest {
+
+    private var requestCode: Int = 1
+    private var resultCode: Int = 2
+
+    @Mock
+    private lateinit var data: Intent
+    @Mock
+    private lateinit var activity: Activity
+    @Mock
+    private lateinit var callbacks: FilePicker.Callbacks
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
+        MockitoAnnotations.initMocks(this)
     }
 
     @After
     @Throws(Exception::class)
     fun tearDown() {
+        for(value in FilePicker.testCoverage)
+            println(value)
     }
 
     @Test
     fun handleActivityResult() {
+        FilePicker.handleActivityResult(requestCode, resultCode, data, activity, callbacks)
     }
 }
