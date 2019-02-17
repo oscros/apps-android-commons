0b72f704654044997b5f59051af66db1099ed6af
Edvin Sparf <esparf@kth.se>
2019-02-17 16:47:36 +0100

    Feat fix #6
    Four tests implemented which increases coverage

--- app/src/test/kotlin/fr/free/nrw/commons/utils/ImageUtilsTest.java
+++ app/src/test/kotlin/fr/free/nrw/commons/utils/ImageUtilsTest.java
@@ -1,18 +1,46 @@
 package fr.free.nrw.commons.utils;
-import org.junit.Assert.assertEquals;
+import org.junit.Assert.assertTrue;
+import org.junit.Assert.assertFalse;
 import org.junit.Test;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import fr.free.nrw.commons.commons.utils.ImageUtils.java;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatcher.any;
 
 public class ImageUtilsTest(){
 
+
   @Test
-  public testCheckIfImageIsDark(){
+  public checkIfImageIsDarkTestNull(){
     //Edge case which should be handled and return true
     assertTrue(checkIfImageIsDark(null));
+  }
 
+  @Test
+  public checkIfImageIsDarkTestZeroes(){
     Bitmap bitmap = mock(Bitmap.class);
-    
+    when(bitmap.getWidth()).thenReturn(0);
+    when(bitmap.getHeight()).thenReturn(0);
+    assertTrue(checkIfImageIsDark(bitmap));
+  }
+
+  @Test
+  public checkIfImageIsDarkTestWhiteImage(){
+    Bitmap bitmap = mock(Bitmap.class);
+    when(bitmap.getWidth()).thenReturn(10);
+    when(bitmap.getHeight()).thenReturn(10);
+    when(bitmap.getPixel(any(), any())).thenReturn(16777215);
+    assertFalse(checkIfImageIsDark(bitmap));
+  }
+
+  @Test
+  public checkIfImageIsDarkTestBlackImage(){
+    Bitmap bitmap = mock(Bitmap.class);
+    when(bitmap.getWidth()).thenReturn(20);
+    when(bitmap.getHeight()).thenReturn(20);
+    when(bitmap.getPixel(any(), any())).thenReturn(0);
+    assertTrue(checkIfImageIsDark(bitmap));
   }
 }
