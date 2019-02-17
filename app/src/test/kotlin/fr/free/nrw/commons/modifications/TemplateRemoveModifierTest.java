07c9f838c942682a52db239ecf8e133c1b1aed0c
Edvin Sparf <esparf@kth.se>
2019-02-17 17:24:48 +0100

    Working on #9
    Set up test class for TemplateRemoveModifier

---
+++ app/src/test/kotlin/fr/free/nrw/commons/modifications/TemplateRemoveModifierTest.java
@@ -0,0 +1,19 @@
+package fr.free.nrw.commons.modifications;
+
+import org.junit.Assert.assertTrue;
+import org.junit.Assert.assertFalse;
+import org.junit.Test;
+import fr.free.nrw.commons.commons.modifications.TemplateRemoveModifier;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatcher.any;
+
+public class TemplateRemoveModifierTest(){
+
+  @Test
+  public void TemplateRemoveModifierTest() {
+    TemplateRemoveModifier trm = mock(TemplateRemoveModifier.class);
+
+  }
+}
